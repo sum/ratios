15489941e213a2fd414768aabb6c2c5ed52a38ac
soldsoul <manysoldsouls@gmail.com>
2014-08-12 23:00:30 -0700

    Updating package

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var fs = require('fs')
-var file = fs.readFileSync(__dirname + '/ratios.css', 'utf8')
+var fs = require('browserify-fs')
+var file
+var insertCSS = require('insert-css')
+fs.readFile('/ratios.css', 'utf-8', function(err, data) {
+    console.log(data)
+})
 
 module.exports = function() {
   return file;
 }
+
